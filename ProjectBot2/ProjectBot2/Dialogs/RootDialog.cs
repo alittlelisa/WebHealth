5a5e473823970f8f44bf43dc2f0675d5e5d8d4b7
Lisa <Lisa@DRAGON>
2018-02-25 17:40:47 -0500

    round 2

---
+++ ProjectBot2/ProjectBot2/Dialogs/RootDialog.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace ProjectBot2.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            // calculate something for us to return
+            int length = (activity.Text ?? string.Empty).Length;
+
+            // return our reply to the user
+           // await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            if (activity.Text == "begin")
+            {
+                await context.PostAsync($"Is your patient happy?");
+            }
+
+            else
+            {
+                await InitialResponse(activity, context);
+            }
+
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task InitialResponse(Activity response, IDialogContext context)
+        {
+            if (response.Text == "no" || response.Text == "negative")
+            {
+                await context.PostAsync($"Your patient needs further diagnostic tests");
+            }
+            else if (response.Text == "yes" || response.Text == "yeah")
+            {
+                await context.PostAsync($"Your patient is probably healthy");
+            }
+        }
+    }
+}
