3e3bf19ce18cdd569463f1152f6741c64780159b
Lisa <Lisa@DRAGON>
2018-02-26 01:28:47 -0500

    integrating LUIS

---
+++ ProjectBot2/ProjectBot2/Dialogs/DefaultDialog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+
+namespace ProjectBot2.Dialogs
+{
+    [LuisModel("{8a4fdce8-e29d-4acd-9c3e-eb2918b6558b}", "{a84b3d6c65c54ec699e8d2a981df1998 , 107ee7391a73465ebecdd521b46a158b}")]
+    [Serializable]
+    public class DefaultDialog : LuisDialog<object>
+    {
+       
+    }
+}
