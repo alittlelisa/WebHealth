2a765b4ca9b246d2eb7d20e560feb9efd8385cd7
Lisa <Lisa@DRAGON>
2018-02-26 19:55:20 -0500

    Implemented all existing intent methods

--- ProjectBot2/ProjectBot2/Dialogs/RootDialog.cs
+++ ProjectBot2/ProjectBot2/Dialogs/RootDialog.cs
@@ -14,14 +14,56 @@ namespace ProjectBot2.Dialogs
         public async Task None(IDialogContext context, LuisResult result)
         {
             await context.PostAsync("I do not understand");
-            //context.Wait(MessageReceived);
+            context.Wait(MessageReceived);
         }
 
         [LuisIntent("Exit")]
         public async Task Quit(IDialogContext context, LuisResult result)
         {
             await context.PostAsync("You want to exit");
-            //context.Wait(MessageReceived);
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("StepBack")]
+        public async Task StepBack(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("You want to return to the last question");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("RestartTree")]
+        public async Task RestartTree(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("You want to restart the diagnostic test");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("MainMenu")]
+        public async Task MainMenu(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("You want to return to the main menu");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("DiagnosticList")]
+        public async Task DiagnosticList(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("You want to go to the assessment menu");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("DenySelection")]
+        public async Task DenySelection(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("You want to cancel/say no");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("ConfirmSelection")]
+        public async Task ConfirmSelection(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("You want to confirm/say yes");
+            context.Wait(MessageReceived);
         }
 
     }
